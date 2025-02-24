39878f3b40ea6457ab59d03c9d6cafb0152b7e63
RajeshDreamTusk <rajesh.r@dreamtusk.com>
2025-02-24 18:20:16 +0530

    Text Button Style changed

--- lib/currency_converter_material.dart
+++ lib/currency_converter_material.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterial extends StatelessWidget{
@@ -53,28 +54,46 @@ class CurrencyConverterMaterial extends StatelessWidget{
                    
                 
                   ),
-                  keyboardType: const TextInputType.numberWithOptions(
-                    decimal: true,
-                    signed: true,
-
-
-
-                  
-                  ),
+                  // keyboardType: const TextInputType.numberWithOptions(
+                  //   decimal: true,
+                  //   signed: true,
+                  // ),
                 ),
               ),
 
-             TextButton(onPressed: () {
-              print("Click here");
-             }, 
-             child: 
-              const Text('Click Here'),
-             )
-
-
+             Padding(
+               padding: const EdgeInsets.all(10.0),
+               child: ElevatedButton(
+                onPressed: () {
+                if(kDebugMode){
+                    print("Click here");
+                    }
+               },
+               style: TextButton.styleFrom(
+                elevation: 50.0,
+                backgroundColor:Color.fromARGB(255, 191, 181, 181),
+                foregroundColor: Color.fromARGB(255, 0, 0, 0),
+                minimumSize: Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.elliptical(10, 10)),
+                )
+              
+               ),
+               // const ButtonStyle(
+                // elevation: WidgetStatePropertyAll(50.0),
+                // backgroundColor:WidgetStatePropertyAll(Color.fromARGB(255, 191, 181, 181)),
+                // foregroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 0, 0, 0)),
+                // minimumSize: WidgetStatePropertyAll(Size(double.infinity, 50)),
+                // shape: WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.elliptical(10, 10))))
+               
+                
+               
+            
+               child: 
+                const Text('Convert'),),
+             ),
           ],
         ),
-      )
+       )
       
     );
   }
