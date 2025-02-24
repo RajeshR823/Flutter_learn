77cef1e71bd7f9e96f54f2c8368cc5c21038941c
RajeshDreamTusk <rajesh.r@dreamtusk.com>
2025-02-24 17:19:42 +0530

    updated Textfiled and button

--- lib/currency_converter_material.dart
+++ lib/currency_converter_material.dart
@@ -5,42 +5,77 @@ class CurrencyConverterMaterial extends StatelessWidget{
 
  @override
   Widget build(BuildContext context) { 
+      final border =  OutlineInputBorder(
+                      borderSide: BorderSide(
+                        // color: Colors.red,
+                        width: 3.0,
+                        style: BorderStyle.solid,
+                        strokeAlign: BorderSide.strokeAlignOutside,
 
-      return const Scaffold(
-        backgroundColor: Colors.blueGrey,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+
+                    );
+      return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 228, 222, 215),
         body:Center(
          child:  Column(
           mainAxisAlignment: MainAxisAlignment.center,
            children: [
-             Text(
-              "Hello there",
+             const Text(
+              "Hello",
               style: TextStyle(
                   fontSize: 35,
                   fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 248, 248, 248), 
+                  color: Color.fromARGB(255, 0, 0, 0), 
+                 
                ),
               ),
-              TextField(
-                style: TextStyle(
-                  color: Colors.amberAccent,
-                ),
-                decoration: InputDecoration(
-                    hintText: 'Enter Here whatever you want',
-                    hintStyle: TextStyle(
-                      color: Colors.black,
-                    ),
-                    // prefixText:'hello', 
-                    prefix: Icon(Icons.insert_emoticon),
-                    filled: true,
-                    fillColor: Colors.white,
-                    focusedBorder:OutlineInputBorder(
-                      
-                  )
+              
+              Padding(
+                padding:const EdgeInsets.all(10),
+            
+                child: TextField(
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  decoration: InputDecoration(
+                      hintText: 'Enter Here whatever you want',
+                      hintStyle: const TextStyle(
+                        color: Colors.black,
+                      ),
+                      // prefixText:'hello', 
+                      // prefix: const Icon(Icons.insert_emoticon),
+                      filled: true,
+                      fillColor: Colors.white,
+                      focusedBorder: border,
+                      enabledBorder: border,
+                   
+                
+                  ),
+                  keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true,
+                    signed: true,
+
+
+
+                  
+                  ),
                 ),
               ),
+
+             TextButton(onPressed: () {
+              print("Click here");
+             }, 
+             child: 
+              const Text('Click Here'),
+             )
+
+
           ],
         ),
       )
+      
     );
   }
 }
