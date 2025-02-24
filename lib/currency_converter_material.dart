a0228360eb00c780cad359bf3e38d15c5d9e595c
RajeshDreamTusk <rajesh.r@dreamtusk.com>
2025-02-24 22:54:00 +0530

    completed currency converter

--- lib/currency_converter_material.dart
+++ lib/currency_converter_material.dart
@@ -1,11 +1,44 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterial extends StatelessWidget{
-    const CurrencyConverterMaterial({super.key});
 
- @override
+
+class CurrencyConverterMaterialPage extends StatefulWidget{
+  const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  State<CurrencyConverterMaterialPage> createState() { 
+    return _CurrencyConverterMaterialPageState();
+
+  } 
+
+
+}
+
+class _CurrencyConverterMaterialPageState extends State<CurrencyConverterMaterialPage>{
+    double result =0;
+
+    final TextEditingController textEditingController = TextEditingController();
+
+    void convert (){
+       result = double.parse(textEditingController.text);
+        setState(() {});
+    }
+
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+
+
+
+     
+    @override
   Widget build(BuildContext context) { 
+    
+  
       final border =  OutlineInputBorder(
                       borderSide: BorderSide(
                         // color: Colors.red,
@@ -18,25 +51,46 @@ class CurrencyConverterMaterial extends StatelessWidget{
 
                     );
       return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 228, 222, 215),
-        body:Center(
-         child:  Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             const Text(
-              "Hello",
-              style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 0, 0, 0), 
-                 
-               ),
-              ),
-              
-              Padding(
-                padding:const EdgeInsets.all(10),
+        backgroundColor: const Color.fromARGB(255, 228, 228, 215),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 226, 226, 134),
+          elevation: 0,
+          title: const Text('Hello World',style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
             
-                child: TextField(
+            ),
+          ),
+          centerTitle: true,
+          actions: [
+            Icon(Icons.ac_unit),
+            Icon(Icons.ac_unit),
+            Icon(Icons.ac_unit),
+
+          ],
+          leading: Icon(Icons.ac_unit_sharp),
+          
+        ),
+        
+        body:Center(
+         child:  Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+                Text(
+                result.toString(),
+                style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 0, 0, 0), 
+                   
+                 ),
+                ),
+                
+                TextField(
+                  controller: textEditingController,
                   style: const TextStyle(
                     color: Color.fromARGB(255, 0, 0, 0),
                   ),
@@ -59,16 +113,9 @@ class CurrencyConverterMaterial extends StatelessWidget{
                   //   signed: true,
                   // ),
                 ),
-              ),
-
-             Padding(
-               padding: const EdgeInsets.all(10.0),
-               child: ElevatedButton(
-                onPressed: () {
-                if(kDebugMode){
-                    print("Click here");
-                    }
-               },
+               const SizedBox(height: 15),
+               ElevatedButton(
+                onPressed: convert,
                style: TextButton.styleFrom(
                 elevation: 50.0,
                 backgroundColor:Color.fromARGB(255, 191, 181, 181),
@@ -76,7 +123,7 @@ class CurrencyConverterMaterial extends StatelessWidget{
                 minimumSize: Size(double.infinity, 50),
                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.elliptical(10, 10)),
                 )
-              
+                             
                ),
                // const ButtonStyle(
                 // elevation: WidgetStatePropertyAll(50.0),
@@ -87,14 +134,34 @@ class CurrencyConverterMaterial extends StatelessWidget{
                
                 
                
-            
+                           
                child: 
-                const Text('Convert'),),
-             ),
-          ],
-        ),
+                const Text('Change'),),
+            ],
+                   ),
+         ),
        )
-      
-    );
+
+
+
+      );
+
   }
 }
+
+
+
+
+
+
+
+// class CurrencyConverterMaterial extends StatelessWidget{
+//     const CurrencyConverterMaterial({super.key});
+
+//  @override
+//   Widget build(BuildContext context) {    
+//       return Scaffold(
+           
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_second_app/cupertino_currency_converter.dart';
 import 'package:my_second_app/currency_converter_material.dart';
 
 void main(){
-  runApp(const myApp());
+  runApp(const MyCupertinoApp());
 }
 class myApp extends StatelessWidget{
   const myApp ({super.key}); //key helps Flutter identify widgets uniquely in the widget tree for efficient updates.
@@ -12,12 +14,27 @@ class myApp extends StatelessWidget{
   ///A handle to the location of a widget in the widget tree.
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:CurrencyConverterMaterial(),
+      home:CurrencyConverterMaterialPage(),
+      
      
     );
   }
 }
 
+class MyCupertinoApp extends StatelessWidget {
+  const MyCupertinoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+     return const  CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home:CupertinoCurrencyConverter(),
+      
+     
+    );
+  }
+}
 
 
 
